8bcabc2eced4369afa9f1fc209e4f8c133d39ffd
MKH103-18 <MKH103-18@lbcc.linnbenton.edu>
2016-11-09 11:41:33 -0800

    Finished template-strings.js

--- template-strings.js
+++ template-strings.js
@@ -1,6 +1,6 @@
-let name = process.argv(2);
+let name = process.argv[2];
 
-let ouput = 'Hello, ${name}!
-Your name lowercased is ""${name.toLowerCase()}".'
+let output = `Hello, ${name}!
+Your name lowercased is "${name.toLowerCase()}".`
 
 console.log(output);
